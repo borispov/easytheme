6ef01f74f6dcebe020157f322884296d2f099b90
Boris Povolotsky <boristofu@gmail.com>
2020-04-26 14:40:40 +0300

    add repo to dev branch

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout title="easytheme | Theming Made Fast & Easy">
+    <div className="bg-gray-500">
+      <h1> Hello World </h1>
+    </div>
+  </Layout>
+)
